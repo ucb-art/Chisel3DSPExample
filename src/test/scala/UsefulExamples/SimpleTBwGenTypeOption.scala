6a0561d2eca27613957eae4ffa43eba3cc44a7ee
Angie <shunshou@gmail.com>
2017-01-24 16:26:41 -0800

    bundle error :( probably b/c of new records stuff in chisel

--- src/test/scala/UsefulExamples/SimpleTBwGenTypeOption.scala
+++ src/test/scala/UsefulExamples/SimpleTBwGenTypeOption.scala
@@ -1,6 +1,7 @@
 package SimpleTB
 
 import chisel3._
+import chisel3.internal.firrtl.{Width, BinaryPoint}
 import chisel3.experimental.FixedPoint
 import chisel3.util.RegNext
 import breeze.math.Complex
@@ -10,54 +11,50 @@ import dsptools.{VerboseDspTester, VerboseDspTesterOptionsManager, VerboseDspTes
 import org.scalatest.{FlatSpec, Matchers}
 import chisel3.iotesters.TesterOptions
 
+
 case class TestParams(
-    val smallW: Int = 4,
-    val bigW: Int = 8,
-    val biggerW: Int = 16,
+    val smallW: Int = 8,
+    val bigW: Int = 16,
     val smallBP: Int = 4,
     val bigBP: Int = 8,
     val vecLen: Int = 4,
     val posLit: Double = 3.3,
     val negLit: Double = -3.3,
-    val lutVals: Seq[Double] = Seq(-3.3, -2.2, -1.1, -0.55, -0.4, 0.4, 0.55, 1.1, 2.2, 3.3),
-    val bp: Int = 4) {
+    val lutVals: Seq[Double] = Seq(-3.3, -2.2, -1.1, -0.55, -0.4, 0.4, 0.55, 1.1, 2.2, 3.3)) {
 
-  val genLongF = FixedPoint(biggerW.W, bigBP.BP)
-  val genShortF = FixedPoint(bigW.W, smallBP.BP)
+  val genLongF = FixedPoint(bigW.W, bigBP.BP)
+  val genShortF = FixedPoint(smallW.W, smallBP.BP)
   val genLongS = SInt(bigW.W)
   val genShortS = SInt(smallW.W)
   val genR = DspReal()
 
 }
 
+class DataTypeBundle[R <: Data:Real](genType: R, width: Width, binaryPoint: BinaryPoint) extends Bundle {
+  val gen = genType.chiselCloneType
+  val s = SInt(width)
+  val f = FixedPoint(width, binaryPoint)
+  val u = UInt(width)
+  override def cloneType: this.type = new DataTypeBundle(gen, width, binaryPoint).asInstanceOf[this.type]
+}
+
 class Interface[R <: Data:Real](genShort: R, genLong: R, includeR: Boolean, p: TestParams) extends Bundle {
 
   val smallW = p.smallW.W
   val bigW = p.bigW.W
-  val biggerW = p.biggerW.W
   val smallBP = p.smallBP.BP
   val bigBP = p.bigBP.BP
   val vecLen = p.vecLen
 
-  val genS = genShort.chiselCloneType
-  val genL = genLong.chiselCloneType
   val r = if (includeR) Some(DspReal()) else None
-  val sS = SInt(smallW)
-  val sL = SInt(bigW)
-  val fS = FixedPoint(bigW, smallBP)
-  val fL = FixedPoint(biggerW, bigBP)
-  val uS = UInt(smallW)
-  val uL = UInt(bigW)
   val b = Bool()
-  val cGenL = DspComplex(genLong, genLong)
-  val cFS = DspComplex(FixedPoint(bigW, smallBP), FixedPoint(bigW, smallBP))
-  val cR = DspComplex(DspReal(), DspReal())
-  val bu = new Bundle {
-    val s = SInt(smallW)
-    val f = FixedPoint(bigW, smallBP)
-    val u = UInt(smallW)
-    val g = genShort.chiselCloneType
-  }
+  val cGenL = DspComplex(genLong)
+  val cFS = DspComplex(FixedPoint(bigW, smallBP))
+  val cR = DspComplex(DspReal())
+
+  val short = new DataTypeBundle(genShort, smallW, smallBP)
+  val long = new DataTypeBundle(genLong, bigW, bigBP)
+
   val vU = Vec(vecLen, UInt(smallW))
   val vS = Vec(vecLen, SInt(smallW))  
   
@@ -69,12 +66,13 @@ class SimpleModule[R <: Data:Real](genShort: R, genLong: R, val includeR: Boolea
   val posLit = p.posLit
   val negLit = p.negLit
   val lutVals = p.lutVals
-  val bp = p.bp
-
+  val bp = p.smallBP
 
   val io = IO(new Bundle {
     val i = Input(new Interface(genShort, genLong, includeR, p))
-    val o = Output(new Interface(genShort, genLong, includeR, p)) })  
+    val o = Output(new Interface(genShort, genLong, includeR, p)) }) 
+
+  //io.o.long <> RegNext(io.i.short) 
 
   val litRP = if (includeR) Some(DspReal(posLit)) else None
   val litRN = if (includeR) Some(DspReal(negLit)) else None
@@ -102,8 +100,8 @@ class SimpleModule[R <: Data:Real](genShort: R, genLong: R, val includeR: Boolea
   val lutGen = Vec(lutGenSeq)
   val lutS = Vec(lutSSeq)
 
-  io.o.genS := lutGen(io.i.uS)
-  io.o.sS := lutS(io.i.uS)
+  io.o.short.gen := lutGen(io.i.short.u)
+  io.o.short.s := lutS(io.i.short.u)
 
 }
 
@@ -148,15 +146,15 @@ class PassLitTester[R <: Data:Real](c: SimpleModule[R]) extends VerboseDspTester
     dspExpect(x, lutVals(i)) }}
 
   c.lutVals.zipWithIndex foreach { case (x, i) => {
-    poke(c.io.i.uS, i)
-    dspPeek(c.io.o.genS)
-    dspExpect(c.io.o.genS, x)
+    poke(c.io.i.short.u, i)
+    dspPeek(c.io.o.short.gen)
+    dspExpect(c.io.o.short.gen, x)
 
     // How to change expect tolerance (for Lits; SInts should match exactly)
     // dspExpect automatically rounds when data is SInt (whereas expect doesn't)
     fixTolLSBs.withValue(0) {
-      dspPeek(c.io.o.sS)
-      dspExpect(c.io.o.sS, x)
+      dspPeek(c.io.o.short.s)
+      dspExpect(c.io.o.short.s, x)
     }
 
   }}
@@ -188,8 +186,8 @@ class FailLitTester[R <: Data:Real](c: SimpleModule[R]) extends VerboseDspTester
     dspExpect(x, lutVals(i)) }}
 
   c.lutVals.zipWithIndex foreach { case (x, i) => {
-    poke(c.io.i.uS, i)
-    dspExpect(c.io.o.genS, x)
+    poke(c.io.i.short.u, i)
+    dspExpect(c.io.o.short.gen, x)
   }}
 
 }
